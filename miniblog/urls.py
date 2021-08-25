30a1755e1977e2546fa1092cf7c1582741c8510e
yhzheng23 <jennyyuhua@gmail.com>
2021-08-25 23:16:23 +0800

    skeleton website

--- miniblog/urls.py
+++ miniblog/urls.py
@@ -15,7 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    path('blog/', include('blog.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+urlpatterns += [
+    path('', RedirectView.as_view(url='/blog/', permanent=True)),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
